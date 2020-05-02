9b6b3af39745c37743e4b122c26930bba34d6648
mgei <martin.geissmann@gmail.com>
2020-05-02 16:08:01 +0200

    caching fct to get payment schedule from mintos

--- helper/get_payment_schedule.R
+++ helper/get_payment_schedule.R
@@ -1,7 +1,7 @@
-library(rvest)
-library(tidyverse)
-
-payment_schedule <- function(loan_number, tibble = F) {
+get_payment_schedule <- function(loan_number, tibble = F) {
+  require(rvest)
+  require(tidyverse)
+  
   url1<-paste0('https://www.mintos.com/webapp/en/', loan_number ,'/?locale=en&referrer=https%3A%2F%2Fwww.mintos.com&hash=&openDoc=false')
   
   table<-read_html(url1) %>%
@@ -33,5 +33,31 @@ payment_schedule <- function(loan_number, tibble = F) {
   
   return(data)
 }
+
+get_payment_schedule_cache <- function(loan_number, tibble = T,
+                                       reload_if_older_than = "1 month", cache_dir = "data/payment_cache/") {
+  
+  if (paste0(loan_number, ".RDS") %in% list.files(cache_dir)) {
+    cached <- readRDS(paste0(cache_dir, loan_number, ".RDS"))
+    
+    if (cached$loaddate + period(reload_if_older_than) >= Sys.Date()) {
+      out <- cached$data
+      
+      return(out)
+    }
+  }
+  
+  # get from Mintos
+  print("get payment schedule from mintos")
+  suppressWarnings(out <- get_payment_schedule(loan_number = loan_number, tibble = tibble))
+  
+  # save to cached files
+  list(loaddate = Sys.Date(),
+       data = out) %>% 
+    saveRDS(paste0(cache_dir, loan_number, ".RDS"))
+  
+  return(out)
+}
+
 
 # payment_schedule("13813937-02", tibble = T)
